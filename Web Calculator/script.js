d0dc0faa65a25871119c0564e6867c025b02f8cc
nirbheek109 <tanimahmed209@gmail.com>
2025-05-20 04:35:40 +0600

    final

--- Web Calculator/script.js
+++ Web Calculator/script.js
@@ -1,18 +1,37 @@
 const loan = document.getElementById("amount");
 const rate = document.getElementById("rate");
 const years = document.getElementById("years");
-const button = document.getElementById("btn");
-const result = document.getElementById("result");
+const cBtn = document.getElementById("calculate");
+const rBtn = document.getElementById("reset");
+const result = document.getElementById("monthlyPayment");
+const result2 = document.getElementById("totalPayment");
+const result3 = document.getElementById("totalInterest");
+const form = document.getElementById(`resetForm`);
+const mother = document.querySelector(`.mother`);
 
-button.addEventListener("click", function () {
-const loanVal = Number(loan.value);
-const rateVal = Number(rate.value) / 12 / 100;
-const year = Number(years.value);
-const month = year * 12;
-const monthlyPay =
-(loanVal * rateVal * Math.pow(1 + rateVal, month)) /
-(Math.pow(1 + rateVal, month) - 1);
-  return (result.innerText = isFinite(monthlyPay)
+function calculateLoan() {
+  mother.style.display = "block";
+  const loanVal = Number(loan.value);
+  const rateVal = Number(rate.value) / 12 / 100;
+  const year = Number(years.value);
+  const month = year * 12;
+  rBtn.style.display = "inline-block";
+  const monthlyPay =
+    (loanVal * rateVal * Math.pow(1 + rateVal, month)) /
+    (Math.pow(1 + rateVal, month) - 1);
+  result.innerText = isFinite(monthlyPay)
     ? `Monthly Payment: $${monthlyPay.toFixed(2)}`
-    : `Please fill all the input field with a valid number.`);
-  });
+    : `Please fill all the input field with a valid number.`;
+  const paymentCount = Number(monthlyPay) * month;
+  result2.innerText = `Total Payment: $${(monthlyPay * month).toFixed(2)}`;
+  const interestCount = `Total Interest: $${(paymentCount - loanVal).toFixed(
+    2
+  )}`;
+  result3.innerText = interestCount;
+  return;
+}
+
+resetForm = () => {
+  form.reset();
+  mother.style.display = "none";
+};
